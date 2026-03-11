78a1ad020a9546eb9d7fa73e6d882397c43ba27c
bimleshkanth <bimleshkanth@gmail.com>
2026-03-11 11:31:54 +0530

    First Commit. Added Shipment domain object and ShipmentService interface

---
+++ src/com/medical/shipment/Shipment.java
@@ -0,0 +1,16 @@
+package com.medical.shipment;
+
+public class Shipment {
+	private String resourceId;
+    private String orderId;
+    private String firstName;
+    private String lastName;
+    private String streetAddress;
+    private String pinCode;
+    private String shipmentDate;
+    private String expiryDate;
+    private String deliveryText;
+    private String deliveryDate;
+    private String trackingNumber;
+    private double chargeAmount;
+}
