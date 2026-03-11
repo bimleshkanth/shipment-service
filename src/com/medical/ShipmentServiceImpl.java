3e1cef6a0be3f6e09973420ee354e8b288b08bc1
bimleshkanth <bimleshkanth@gmail.com>
2026-03-11 12:18:15 +0530

    Implemented in-memory shipment storage using HashMap

--- src/com/medical/ShipmentServiceImpl.java
+++ src/com/medical/ShipmentServiceImpl.java
@@ -1,14 +1,23 @@
+import java.util.HashMap;
+import java.util.Map;
+
 package com.medical;
 
 public class ShipmentServiceImpl implements ShipmentService {
+	
+	private Map<String, Shipment> shipmentDB = new HashMap<>();
+	
+	@Override
 	public void addShipment(Shipment shipment) {
         System.out.println("Shipment added");
     }
-
+	
+	@Override
     public void deleteShipment(String resourceId) {
         System.out.println("Shipment deleted");
     }
-
+	
+	@Override
     public Shipment getShipment(String resourceId) {
         return null;
     }
